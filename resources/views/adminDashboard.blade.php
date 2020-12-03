4b2302a66d9aa5ae7d866a25443499e437f2485d
fxdavidh <fxdavidh@gmail.com>
2020-12-04 01:59:07 +0700

    Fixed Admin Dashboard UI

--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header ">{{ __('Admin Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,34 +13,51 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <thead>
-
-                    </thead>
-                    <tbody>
-                        @foreach ($umkms as $key => $umkm)
-                            <tr>
-                                <td>{{$umkm -> owner}}</td>
-                                <td>{{$umkm -> idNumber}}</td>
-                                <td>{{$umkm -> dob}}</td>
-                                <td>{{$umkm -> title}}</td>
-                                <td>{{$umkm -> field}}</td>
-                                <td>{{$umkm -> description}}</td>
-                                <td>{{$umkm -> address}}</td>
-                                <td>{{$umkm -> district}}</td>
-                                <td>{{$umkm -> number}}</td>
-                                <td><img src="{{asset('storage/'.$umkm -> image)}}" width="100px" alt="{{'image'.$key}}"></td>
-                                <td>{{$umkm -> verified}}</td>
-                                <td>
-                                    <a href="{{route('showUpdate', ['id' => $umkm->id])}}">Edit</a>
-                                    <form action="{{route('umkmDelete', ['id' => $umkm->id])}}" method="POST" enctype="multipart/form-data">
-                                        @csrf
-                                        @method('delete')
-                                        <button>Delete</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">No.KTP</th>
+                                    <th scope="col">Tanggal Lahir</th>
+                                    <th scope="col">Nama UMKM</th>
+                                    <th scope="col">Bidang UMKM</th>
+                                    <th scope="col">Deskripsi</th>
+                                    <th scope="col">Alamat</th>
+                                    <th scope="col">Kecamatan</th>
+                                    <th scope="col">No WA</th>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Verifikasi</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($umkms as $key => $umkm)
+                                    <tr>
+                                        <td>{{$umkm -> owner}}</td>
+                                        <td>{{$umkm -> idNumber}}</td>
+                                        <td>{{$umkm -> dob}}</td>
+                                        <td>{{$umkm -> title}}</td>
+                                        <td>{{$umkm -> field}}</td>
+                                        <td>{{$umkm -> description}}</td>
+                                        <td>{{$umkm -> address}}</td>
+                                        <td>{{$umkm -> district}}</td>
+                                        <td>{{$umkm -> number}}</td>
+                                        <td><img src="{{asset('storage/'.$umkm -> image)}}" width="100px" alt="{{'image'.$key}}"></td>
+                                        <td>{{$umkm -> verified}}</td>
+                                        <td>
+                                            <a href="{{route('showUpdate', ['id' => $umkm->id])}}" class="btn btn-success">Edit</a>
+                                            <form action="{{route('umkmDelete', ['id' => $umkm->id])}}" method="POST" enctype="multipart/form-data">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
